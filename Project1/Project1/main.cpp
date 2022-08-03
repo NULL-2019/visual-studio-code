d1c03d151861a913d4ae62c982051545a87c8b0d
qz <1875971807@qq.com>
2022-08-03 12:45:02 +0800

    add new  fuction diff()

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 
 using namespace std;
+int deff()
+{
+	cout << "in fuction diff() " << endl;
+	return 0;
+}
+
 
 int main(void)
 {
 	cout << "hello " << endl;
+	deff();
 
 	return 0;
 }
